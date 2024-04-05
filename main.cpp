215bdcf2148e9e633a1e27ee3dd4378a4bc5b841
akmlrafly <akmalrafly875@gmail.com>
2024-04-05 20:00:32 +0700

    "second commit"

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
+    cout << "=========================================" << endl;
+    cout << "SELAMAT DATANG DI TOKO SEPATU ABIBAS KAMI" << endl;
+    cout << "=========================================" << endl;
+    cout << endl;
+    cout << "=========================================" << endl;
+    cout << "              Menu login                 " << endl;
+    cout << "=========================================" << endl;
 
     return 0;
 }
