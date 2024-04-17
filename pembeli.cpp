12a6bff644c00c2ee149b23099b1f3fdea99f423
akmlrafly <akmalrafly875@gmail.com>
2024-04-17 11:26:04 +0700

    oke

---
+++ pembeli.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void pembeli(){
+    string username, password;
+
+    // login pembeli
+    cout << "=============================================" << endl;
+    cout << "                HELLO FRIENDS!               " << endl;
+    cout << "=============================================" << endl;
+    cout << endl;
+    cout << "Silahkan Masukkan Username dan Password anda" << endl;
+    cout << "Username* (no spasi): ";
+    cin >> username;
+    cout << "Password* (no spasi): ";
+    cin >> password;
+    cout << endl;
+    cout << "Akun Anda Berhasil Dibuat..." << endl;
+    cout << endl;
+}
