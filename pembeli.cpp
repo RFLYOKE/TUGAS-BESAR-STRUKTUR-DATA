1404dea897f0a21642a6f81f0d2db7e128a6fd5e
akmlrafly <akmalrafly875@gmail.com>
2024-04-27 12:51:42 +0700

    lanjutt broo

--- pembeli.cpp
+++ pembeli.cpp
@@ -12,67 +12,91 @@ struct barang
 {
     int tempat;
     string data[maksimal];
-}; barang sepatu;
+};
+barang sepatu;
 
-
-void inisialisasi(){
+void inisialisasi()
+{
     sepatu.tempat = -1;
 }
 
-bool barangBelanjaKosong(){
-    if(sepatu.tempat == -1){
+bool barangBelanjaKosong()
+{
+    if (sepatu.tempat == -1)
+    {
         return true;
-    } else{
+    }
+    else
+    {
         return false;
     }
 }
 
-bool barangBelanjaPenuh(){
-    if(sepatu.tempat == maksimal -1){
+bool barangBelanjaPenuh()
+{
+    if (sepatu.tempat == maksimal - 1)
+    {
         return true;
-    } else{
+    }
+    else
+    {
         return false;
     }
 }
 
-void pushBarang(string dataMasuk){
-    if(barangBelanjaKosong() == true){
+void pushBarang(string namaSepatu)
+{
+    if (barangBelanjaKosong() == true)
+    {
         sepatu.tempat++;
-        sepatu.data[sepatu.tempat] = dataMasuk;
-        cout << "Berhasil Menambahkan sepatu nomer " << dataMasuk << " ke dalam keranjang" << endl;
-    } else if(barangBelanjaPenuh() == false){
+        sepatu.data[sepatu.tempat] = namaSepatu;
+        cout << "Berhasil Menambahkan sepatu " << namaSepatu << " ke dalam keranjang" << endl;
+    }
+    else if (barangBelanjaPenuh() == false)
+    {
         sepatu.tempat++;
-        sepatu.data[sepatu.tempat] = dataMasuk;
-        cout << "Berhasil Menambahkan sepatu nomer " << dataMasuk << " ke dalam keranjang" << endl;
-    } else {
+        sepatu.data[sepatu.tempat] = namaSepatu;
+        cout << "Berhasil Menambahkan sepatu " << namaSepatu << " ke dalam keranjang" << endl;
+    }
+    else
+    {
         cout << "Barang Anda Terlalu Banyak Silahkan Datang Kembali Besok" << endl;
     }
 }
 
-void popBarang(){
-    if(barangBelanjaKosong() == true){
+void popBarang()
+{
+    if (barangBelanjaKosong() == true)
+    {
         cout << "Belum Ada Barang Yang Anda Beli" << endl;
-    } else{
+    }
+    else
+    {
         sepatu.tempat--;
         cout << "Barang Berhasil Di Cancel" << endl;
     }
 }
 
-void tampilanPembelian(){
-    if(barangBelanjaKosong() == true){
+void tampilanPembelian()
+{
+    if (barangBelanjaKosong() == true)
+    {
         cout << "Tidak Ada Barang Belanja" << endl;
-    } else{
-        for(int i=0; i < sepatu.tempat; i++){
+    }
+    else
+    {
+        for (int i = 0; i <= sepatu.tempat; i++)
+        {
             cout << sepatu.data[i] << endl;
         }
     }
 }
 
-
-void loginPembeli(){
+void loginPembeli()
+{
     string username, password, usernameLogin, passwordLogin;
-    
-    int maksimal = 0; 
+
+    int maksimal = 0;
     bool loginSukses = false;
     int pilih;
 
@@ -90,8 +114,9 @@ void loginPembeli(){
     cout << endl;
     cout << "Akun Anda Berhasil Dibuat..." << endl;
     cout << endl;
-    
-    do {
+
+    do
+    {
         cout << "=============================================" << endl;
         cout << "                LOGIN PEMBELI                " << endl;
         cout << "=============================================" << endl;
@@ -102,99 +127,142 @@ void loginPembeli(){
         cin >> passwordLogin;
         cout << endl;
 
-        if(usernameLogin == username && passwordLogin == password) {
+        if (usernameLogin == username && passwordLogin == password)
+        {
             cout << "Login Berhasil!" << endl;
             loginSukses = true;
 
-            do{
-            cout << "====================================" << endl;
-            cout << "|           MENU PEMBELI            |" << endl;
-            cout << "| 1.| List Sepatu Tersedia          |" << endl;
-            cout << "| 2.| Pembatalan pembelian          |" << endl;
-            cout << "| 3.| Rekapan Pembelian             |" << endl;
-            cout << "| 0.| Keluar                        |" << endl;
-            cout << "====================================" << endl;
-            cout << "Silahkan masukkan pilihan anda: ";
-            cin >> pilih;
-            cout << endl;
-
-            if (pilih == 1)
+            do
             {
-                int pilihanku;
                 cout << "====================================" << endl;
-                cout << "|             KATEGORI              |" << endl;
-                cout << "| 1.| Sepatu Sepak Bola             |" << endl;
-                cout << "| 2.| Sepatu Basket                 |" << endl;
-                cout << "| 3.| Sepatu Running                |" << endl;
-                cout << "| 4.| Sepatu Sekolah                |" << endl;
+                cout << "|           MENU PEMBELI            |" << endl;
+                cout << "| 1.| List Sepatu Tersedia          |" << endl;
+                cout << "| 2.| Pembatalan pembelian          |" << endl;
+                cout << "| 3.| Rekapan Pembelian             |" << endl;
+                cout << "| 4.| Pembayaran                    |" << endl;
                 cout << "| 0.| Keluar                        |" << endl;
                 cout << "====================================" << endl;
-                cout << "Silahkan pilih kategori anda: ";
-                cin >> pilihanku;
+                cout << "Silahkan masukkan pilihan anda: ";
+                cin >> pilih;
                 cout << endl;
-                switch (pilihanku)
+
+                if (pilih == 1)
                 {
-                case 1:
-                    int pilih;
-                    cout << "================================" << endl;
-                    cout << "     DAFTAR SEPATU SEPAK BOLA   " << endl;
-                    cout << "================================" << endl;
-                    cout << "| 1.| PREDATOR 24 ELITE LEKLESS |" << endl;
-                    cout << "| 2.| SAMBA INTER MIAMI CF      |" << endl;
-                    cout << "| 3.| X CRAZYFAST ELITE FIRM    |" << endl;
-                    cout << "| 4.| PREDATOR LEAGUE FIRM      |" << endl;
-                    cout << "| 5.| PREDATOR 24 GROUND BOOTS  |" << endl;
-                    cout << "| 6.| COPA GLORD LEAGUE FIRM    |" << endl;
-                    cout << "| 7.| COLekLessPA GLORD 45      |" << endl;
-                    cout << "| 8.| X CRAZYFAST LEAGUE LEKLESS|" << endl;
-                    cout << "================================" << endl;
-                    do{
-                    cout << "Pilih Sepatu Anda (1/2/3/4/5/6/7/8): ";
-                    cin >> pilih;
-                    pushBarang(dataMasuk);
-                    cout << "Apakah anda ingin memilih lagi? (y/t)";
-                    cin >> yakin;
-                    } while (yakin == 'y' || yakin == 'Y');
-                    cout << "Silahkan Melakukan Pembayaran" << endl;
-                    break;
-                case 2:
-                    
-                    break;
-                case 3:
-                    
-                    break;
-                case 4:
-                   
-                    break;
-                case 0:
-                    cout << "-----KELUAR-----\n";
-                    break;
-                default:
-                    cout << "Pilihan yang anda masukkan salah, coba lagi \n";
-                    break;
+                    int pilihanku;
+                    cout << "====================================" << endl;
+                    cout << "|             KATEGORI              |" << endl;
+                    cout << "| 1.| Sepatu Sepak Bola             |" << endl;
+                    cout << "| 2.| Sepatu Basket                 |" << endl;
+                    cout << "| 3.| Sepatu Running                |" << endl;
+                    cout << "| 4.| Sepatu Sekolah                |" << endl;
+                    cout << "| 0.| Keluar                        |" << endl;
+                    cout << "====================================" << endl;
+                    cout << "Silahkan pilih kategori anda: ";
+                    cin >> pilihanku;
+                    cout << endl;
+                    switch (pilihanku)
+                    {
+                    case 1:
+                        int nomer;
+                        cout << "================================" << endl;
+                        cout << "     DAFTAR SEPATU SEPAK BOLA   " << endl;
+                        cout << "================================" << endl;
+                        cout << "| 1.| PREDATOR 24 ELITE LEKLESS |" << endl;
+                        cout << "| 2.| SAMBA INTER MIAMI CF      |" << endl;
+                        cout << "| 3.| X CRAZYFAST ELITE FIRM    |" << endl;
+                        cout << "| 4.| PREDATOR LEAGUE FIRM      |" << endl;
+                        cout << "| 5.| PREDATOR 24 GROUND BOOTS  |" << endl;
+                        cout << "| 6.| COPA GLORD LEAGUE FIRM    |" << endl;
+                        cout << "| 7.| COLekLessPA GLORD 45      |" << endl;
+                        cout << "| 8.| X CRAZYFAST LEAGUE LEKLESS|" << endl;
+                        cout << "================================" << endl;
+                        do
+                        {
+                            cout << "Pilih Sepatu Anda (1/2/3/4/5/6/7/8): ";
+                            cin >> nomer;
+                            string namaSepatu;
+                            switch (nomer)
+                            {
+                            case 1:
+                                namaSepatu = "PREDATOR 24 ELITE LEKLESS";
+                                break;
+                            case 2:
+                                namaSepatu = "SAMBA INTER MIAMI CF";
+                                break;
+                            case 3:
+                                namaSepatu = "X CRAZYFAST ELITE FIRM";
+                                break;
+                            case 4:
+                                namaSepatu = "PREDATOR LEAGUE FIRM";
+                                break;
+                            case 5:
+                                namaSepatu = "PREDATOR 24 GROUND BOOTS";
+                                break;
+                            case 6:
+                                namaSepatu = "COPA GLORD LEAGUE FIRM";
+                                break;
+                            case 7:
+                                namaSepatu = "COLekLessPA GLORD 45";
+                                break;
+                            case 8:
+                                namaSepatu = "X CRAZYFAST LEAGUE LEKLESS";
+                                break;
+                            default:
+                                namaSepatu = "Pilihan tidak valid";
+                                break;
+                            }
+                            pushBarang(namaSepatu);
+                            cout << "Apakah anda ingin memilih lagi? (y/t)";
+                            cin >> yakin;
+                        } while (yakin == 'y' || yakin == 'Y');
+                        cout << "Silahkan Melakukan Pembayaran" << endl;
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 0:
+                        cout << "-----KELUAR-----\n";
+                        break;
+                    default:
+                        cout << "Pilihan yang anda masukkan salah, coba lagi \n";
+                        break;
+                    }
                 }
-            }
-            else if (pilih == 2)
-            {
-                popBarang();
-            }
-            else if (pilih == 3)
-            {
-                listSepatu();
-            }
-            else
-            {
-                cout << " Terima Kasih Sudah Datang " << endl;
-            }
-        } while(keluar == false);
-        } else {
+                else if (pilih == 2)
+                {
+                    popBarang();
+                }
+                else if (pilih == 3)
+                {
+                    listSepatu();
+                }
+                else if (pilih == 4)
+                {
+                    cout << "=========================" << endl;
+                    cout << "     Menu Pembayaran     " << endl;
+                    cout << "=========================" << endl;
+                }
+                else
+                {
+                    cout << " Terima Kasih Sudah Datang " << endl;
+                }
+            } while (keluar == false);
+        }
+        else
+        {
             maksimal++;
             cout << "Username atau Password salah, silahkan coba lagi." << endl;
-            if(maksimal >= 3) {
+            if (maksimal >= 3)
+            {
                 cout << "Anda telah mencoba login sebanyak 3 kali. Silahkan coba lagi nanti." << endl;
                 break;
             }
         }
     } while (!loginSukses && maksimal < 3);
 }
-
